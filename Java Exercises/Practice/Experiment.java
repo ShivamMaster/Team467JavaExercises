83264d5dc40e4cef693acd6485f7e0e3741c1691
Shivam Goel <shivam0922@gmail.com>
2021-11-02 20:19:44 -0400

    tyr to upload

--- Java Exercises/Practice/Experiment.java
+++ Java Exercises/Practice/Experiment.java
@@ -5,6 +5,7 @@ public class Experiment {
     public static void main(final String[] arg)
     {
         double d=78.9087777;
+        double d2=5.0;
             
     System.out.println ( Math.round(d*100.0)/100.0 );
     }
